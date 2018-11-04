bafd42daffc2df0a9bf6c5e565a5232f1489b08a
Kaelinator <kirkkael@gmail.com>
2018-11-03 22:38:58 -0600

    working camera

--- lib/views/crime.dart
+++ lib/views/crime.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:camera/camera.dart';
 
 class CrimeView extends StatefulWidget {
   @override
@@ -6,10 +7,51 @@ class CrimeView extends StatefulWidget {
 }
 
 class _CrimeViewState extends State<CrimeView> {
+
+  CameraController controller;
+  List<CameraDescription> cameras;
+
+  Future<List<CameraDescription>> getCameras() {
+    
+    return availableCameras();
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCameras()
+      .then((List<CameraDescription> s) {
+        setState(() {
+          cameras = s;
+        });
+      })
+      .then((_) {
+        controller = CameraController(cameras[0], ResolutionPreset.high);
+        controller.initialize().then((_) {
+          if (!mounted) return;
+
+          setState(() {});
+        });
+      });
+
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hello there")
-    );
+    if (!controller.value.isInitialized) {
+      return new Container();
+    }
+    return new AspectRatio(
+        aspectRatio:
+        controller.value.aspectRatio,
+        child: new CameraPreview(controller));
   }
 }
