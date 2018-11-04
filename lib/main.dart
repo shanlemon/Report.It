28b3c7d4512fb44d7178b858c40ddcd9e81fc316
Dosmaster4life <jefferysuperman@gmail.com>
2018-11-03 22:45:02 -0500

    error

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,17 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
+import './Files/Camera.dart';
 
-List<CameraDescription> cameras;
+void main() => runApp(new MyApp());
 
-Future<Null> main() async {
-  cameras = await availableCameras();
-  runApp(new CameraApp());
-}
-
-class CameraApp extends StatefulWidget {
-  @override
-  _CameraAppState createState() => new _CameraAppState();
-}
-
-class _CameraAppState extends State<CameraApp> {
-  CameraController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = new CameraController(cameras[0], ResolutionPreset.medium);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return new Container();
-    }
-    return new AspectRatio(
-        aspectRatio:
-        controller.value.aspectRatio,
-        child: new CameraPreview(controller));
+    return new MaterialApp(
+      title: 'Report It',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new CameraApp(),
+    );
   }
 }
